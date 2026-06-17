1d0be04d0bfbd86576877254c07462f0a4b01dad
Hossam Ayman <hhasanelbadry2000@gmail.com>
2026-06-17 16:18:39 +0300

    second commit

--- src/main/java/com/example/springwithazuredemo/controller/DemoController.java
+++ src/main/java/com/example/springwithazuredemo/controller/DemoController.java
@@ -8,7 +8,7 @@ public class DemoController {
 
     @GetMapping("/message")
     public String message() {
-        return "Congrats ! your app deployed successfully in Azure !";
+        return "Congrats ! your application deployed successfully in Azure Platform !";
     }
 
 }
